b5f27577f0d2f35640dd96640bdc7aa985a61999
AnBANSAL-cmpm <abansal6@ucsc.edu>
2025-10-03 14:22:13 -0700

    state counter - string

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   state.counter++;
-  counterElement.textContent = state.counter;
+  counterElement.textContent = state.counter.toString();
   console.log("I have these thingies:", button, counterElement, state.counter);
 });
